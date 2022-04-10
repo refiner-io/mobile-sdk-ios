d77c6d922ad46b13fb130ca726cd616fa91d23bc
aniltaskiran <aniltaskiran@outlook.com.tr>
2022-04-10 16:46:15 +0300

    add RefinerSDK

---
+++ RefinerSDK/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "RefinerSDK",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "RefinerSDK",
+            targets: [
+                "RefinerSDK",
+                "RefinerInternalSDK"
+            ]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "RefinerSDK",
+            path: "./RefinerSDK.xcframework"
+        ),
+        .binaryTarget(
+            name: "RefinerInternalSDK",
+            path: "./RefinerInternalSDK.xcframework"
+        ),
+    ]
+)
